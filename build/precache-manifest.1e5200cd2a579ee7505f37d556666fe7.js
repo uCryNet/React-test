e1cc6d788d46695f3f39708c3dc92bd54f46b05b
CryNet <ucrynet@gmail.com>
2020-08-29 15:24:35 +0300

    29.08.2020 15:24

---
+++ build/precache-manifest.1e5200cd2a579ee7505f37d556666fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1c42abb27c6ff9fde563b34c36853a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0d7e18b4ebee4da5ad2",
+    "url": "/static/css/main.641325c2.chunk.css"
+  },
+  {
+    "revision": "d46a40d870020de53015",
+    "url": "/static/js/2.f17ba730.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/static/js/2.f17ba730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d7e18b4ebee4da5ad2",
+    "url": "/static/js/main.df182430.chunk.js"
+  },
+  {
+    "revision": "49724680fb5eab8cf2b0",
+    "url": "/static/js/runtime-main.de082351.js"
+  }
+]);
